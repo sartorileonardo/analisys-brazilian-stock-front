40dd694211ceadba4d002ac8d172fa9b823a93d0
leonardogt4 <leonardogt4@hotmail.com>
2022-06-22 19:54:18 -0300

    [Updated] - Deploy configs

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname,'./dist/analisys-brazilian-stock-front')));
+app.use(express.static('./dist/analisys-brazilian-stock-front'));
 
-app.get('/', (req, res) =>
-    res.sendFile('dist/analisys-brazilian-stock-front/index.html', {root: 'dist/analisys-brazilian-stock-front/'}),
-);
+app.get('/*', function(req,res) {
+
+  if(req.url === '/') {
+    res.sendFile(path.join(__dirname,'/dist/analisys-brazilian-stock-front/index.html'));
+  }
+  else {
+    res.sendFile(path.join(__dirname, '/dist/analisys-brazilian-stock-front' + req.url))
+  }
+
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
