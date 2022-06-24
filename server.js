23a868dc71e594ed56dbfb8bddd658287d2167f6
leonardogt4 <leonardogt4@hotmail.com>
2022-06-23 21:27:53 -0300

    [Updated] - Updated heroku deploy configs

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/analisys-brazilian-stock-front'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/analisys-brazilian-stock-front/index.html'));
 });
 
 app.listen(process.env.PORT || 4200);
