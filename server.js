be64082a33bb4beb6df5a8117d17124c9b99054b
leonardogt4 <leonardogt4@hotmail.com>
2022-06-23 21:17:24 -0300

    [Updated] - Updated heroku deploy configs

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+
 
 app.use(express.static(__dirname + '/dist/analisys-brazilian-stock-front'));
 
-// Link index.html of build folder with router.
-app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/analisys-brazilian-stock-front/' }
-    );
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/dist/analisys-brazilian-stock-front/index.html'));
 });
 
- 
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+app.listen(process.env.PORT || 8080);
