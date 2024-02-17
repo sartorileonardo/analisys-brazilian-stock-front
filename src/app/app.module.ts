d8de0b562f3dafcda67304f9cc9a161d4f62a10e
Leonardo Sartori <leonardogt4@hotmail.com>
2024-02-16 23:12:45 -0300

    Add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnalisysBrazilianStockComponent } from './analisys-brazilian-stock/analisys-brazilian-stock.component';
+import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
     AnalisysBrazilianStockComponent
   ],
   imports: [
